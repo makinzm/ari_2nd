eb67d543575eb9101efba688d9c9338ab8b1abe1
makinzm <yuruseba@yahoo.co.jp>
2021-03-22 20:59:41 +0900

    poj3187

---
+++ 1_full_search/all_enumeration/poj3187.py
@@ -0,0 +1,32 @@
+"""
+problem URL: http://poj.org/problem?id=3187
+
+Time:20:43-20:57
+
+苦戦しなかった理由: 再帰構造で簡単に解けることがわかったから.
+"""
+
+n,f=map(int,input().split())
+
+import itertools
+
+def calc(l,m):
+    """
+    長さmのあるリストが入ってきたときのfinalを計算する
+    """
+    nl=[]
+    if m==1:
+        return l[0]
+    else:
+        for i in range(m-1):
+            nl.append(l[i]+l[i+1])
+        return calc(nl,m-1)
+
+for x in itertools.permutations(range(1,n+1)):
+    final=calc(list(x),n)
+    if final==f:
+        ans=x
+        break
+
+[print(x,end=' ') for x in ans]
+print()
