6eefcd98eb65dfeba1abc420170874c509c641fe
makinzm <yuruseba@yahoo.co.jp>
2021-03-24 15:29:41 +0900

    true_ans

--- 3_dp/basic/sumset_ans.py
+++ 3_dp/basic/sumset_ans.py
@@ -13,12 +13,12 @@ i=1
 ## dp[x]: xの答えを返す.
 ## 0を設定するのは,j-iが0の時に1を足すようにする必要があるため.
 while(i<=n):
-    print('===={}===='.format(i))
+    #print('===={}===='.format(i))
     for j in range(i,n+1):
         dp[j] += dp[j-i]
         dp[j] %=MOD
-        print(dp)
-    print('===={}===='.format(i))
+        #print(dp)
+    #print('===={}===='.format(i))
     i*=2
 print(dp[n])
 
@@ -27,4 +27,5 @@ print(dp[n])
 1loop目で2^0=1を使う通りを全て確かめ
 2loop目で2^1を一つ以上使う通りを全て確かめてといったように,
 重なりを無くして足し合わせることができる.
+しかし,これは O(Sum(n))=O(n^2(log n))のためオーダー的にOUT
 """
